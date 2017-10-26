d1c912ce9ffac51560b299a6f1b3996bcc649767
liwei <liwei72439936@gmail.com>
2017-10-26 22:35:54 +0800

    not work

--- lab2/chat-1.0.0/src/Chat.hs
+++ lab2/chat-1.0.0/src/Chat.hs
@@ -8,27 +8,41 @@ import           Network.Socket
 import           System.Environment
 import           System.IO
 
+type UserName = Int
+
 tcpListenOn :: IO Socket
 tcpListenOn = do
   let hints = defaultHints { addrFlags = [AI_NUMERICHOST], addrSocketType = Stream }
   port <- getEnv "CHAT_SERVER_PORT"
   addr:_ <- getAddrInfo (Just hints) (Just "0.0.0.0") (Just port)
-  socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  s <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  bind s (addrAddress addr)
+  listen s 5
+  return s
 
-chooseUserName :: Num a => MVar a -> IO a
+userNameStart :: UserName
+userNameStart = 1
+
+chooseUserName :: MVar UserName -> IO UserName
 chooseUserName mi = modifyMVar mi $ \prev -> return (prev+1, prev)
 
-receiveConnect :: Socket -> MVar [Handle] -> IO ()
-receiveConnect s handles = bracket (socketToHandle s ReadWriteMode) hClose
+receiveConnect :: Socket -> UserName -> MVar [Handle] -> IO ()
+receiveConnect s userName handles = bracket (socketToHandle s ReadWriteMode) hClose
                            $ \h -> do modifyMVar_ handles (\hs -> return $ h:hs)
-                                      forkIO $ forever $ do inp <- hGetLine h
-                                                            hs <- takeMVar handles
-                                                            let others = filter (/= h) hs
-                                                            forM_ others (flip hPutStrLn inp)
+                                      hs <- takeMVar handles
+                                      forM_ hs (flip hPutStrLn $ show userName ++ " has joined.")
+                                      _ <- forkIO $ forever $
+                                           do inp <- hGetLine h
+                                              takeMVar handles >>= mapM_ (flip hPutStrLn $ show userName ++ ": " ++ inp) . filter (/=h)
                                       return ()
 
 
 -- | Chat server entry point.
 chat :: IO ()
-chat = return ()
-
+chat = do
+  s <- tcpListenOn
+  uNameM <- newMVar userNameStart
+  handlesM <- newMVar []
+  forever $ do (ns, _) <- accept s
+               uName <- chooseUserName uNameM
+               receiveConnect ns uName handlesM
