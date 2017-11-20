47fc84384702854491385bc81aad924ccc084d79
liwei <liwei72439936@gmail.com>
2017-11-20 22:56:46 +0800

    compare main logic

--- lab3/trahs-1.0.0/src/Trahs.hs
+++ lab3/trahs-1.0.0/src/Trahs.hs
@@ -1,5 +1,61 @@
-module Trahs (trahs) where
+module Trahs (trahs, compareHistory, compareFile, CompareResult(..)) where
+
+import Data.List
+import Data.Set (fromList, toList)
 
 trahs :: IO ()
 trahs = return ()
 
+data CompareResult = Prefix | Equal | Postfix | Conflict | Delete | Create
+  deriving (Show, Eq)
+
+type FileName = String
+
+type History = [String]
+type FileInfo = (FileName, History)
+type FileInfos = [FileInfo]
+
+compareFile :: FileInfos -> FileInfos -> [(FileName,History,CompareResult)]
+compareFile this that = let
+  allFiles = toList $ fromList $ map fst (this ++ that)
+  help Nothing (Just (f,h)) = (f,h,Create)
+  help (Just (f,h)) Nothing = (f,h,Delete)
+  help (Just (f,h1)) (Just (_,h2)) = (f,h1,compareHistory h1 h2)
+  help _ _ = error "compare not full"
+  in
+  flip map allFiles $ \f -> help (find (\(f1, _) -> f == f1) this)
+                       (find (\(f2, _) -> f == f2) that)
+
+compareHistory :: History -> History -> CompareResult
+compareHistory hs1 hs2 = let
+  zipRes = zipWith (==) hs1 hs2
+  l1 = length hs1
+  l2 = length hs2
+  in
+    if (all id zipRes)
+    then if (l1 == l2)
+         then Equal
+         else if (l1 < l2)
+              then Prefix
+              else Postfix
+    else Conflict
+
+sync :: CompareResult -> IO ()
+sync Equal = return ()
+sync Prefix = syncFromOther
+sync Postfix = return ()
+sync Conflict = reportConflict
+
+syncFromOther :: IO ()
+syncFromOther = return ()
+
+reportConflict :: IO ()
+reportConflict = return ()
+
+handleConflict :: History -> History -> IO ()
+handleConflict hs1 hs2 = do
+  writeHistorySelf
+  readFromOther
+
+writeHistorySelf = return ()
+readFromOther = return ()
--- lab3/trahs-1.0.0/test/Test.hs
+++ lab3/trahs-1.0.0/test/Test.hs
@@ -14,8 +14,26 @@ main = hspec $ describe "Testing Lab 3" $ do
       x <- trahs
       x `shouldBe` ()
 
+  describe "compareHistory" $ do
+    it "prefix" $ do
+      (compareHistory ["rectangle"] ["rectangle", "triangle"]) `shouldBe` Prefix
+    it "postfix" $ do
+      (compareHistory ["rectangle", "triangle"] ["rectangle"]) `shouldBe` Postfix
+    it "Conflict" $ do
+      (compareHistory ["rectangle", "triangle"] ["rectangle", "start"]) `shouldBe` Conflict
+    it "equal" $ property $
+      \x -> compareHistory x x == Equal
+
+  describe "compareFile" $ do
+    it "should get empty when input empty" $ do
+      compareFile [] [] `shouldBe` []
+    it "should get each situation" $ do
+      compareFile [("new", [""]), ("")]
+
   -- example quickcheck test in hspec.
   describe "read" $ do
     it "is inverse to show" $ property $
       \x -> (read . show) x == (x :: Int)
+
+
 
