7936433bf23cbcdf84a99426842ac2045bf836da
liwei <weili@thoughtworks.com>
2017-12-12 18:12:20 +0800

    stash

--- lab3/trahs-1.0.0/src/Trahs.hs
+++ lab3/trahs-1.0.0/src/Trahs.hs
@@ -9,7 +9,7 @@ import qualified Data.ByteString.Lazy     as L
 import           Data.Int
 import           Data.List
 import qualified Data.Map.Strict          as Map
-import           Data.Maybe               (fromJust)
+import           Data.Maybe               (fromJust,maybeToList)
 import           System.Directory
 import           System.Environment
 import           System.Exit
@@ -33,10 +33,10 @@ type VersionInfo = Map.Map (FilePath, ReplicaId) Version
 type FileInfo = Map.Map FilePath String
 
 data Database = Database {
-    _replicaId :: ReplicaId
-  , _versionId    :: Version
-  , _dbVersionInfo    :: VersionInfo
-  , _dbFileInfo       :: FileInfo
+    _replicaId     :: ReplicaId
+  , _versionId     :: Version
+  , _dbVersionInfo :: VersionInfo
+  , _dbFileInfo    :: FileInfo
   } deriving (Show, Read, Eq)
 
 makeLenses ''Database
@@ -63,14 +63,22 @@ mergeLocalInfo :: Database -> FileInfo -> Database
 mergeLocalInfo (Database rid vid vis fis) nfis =
   Database rid vid finalVis nfis
   where
-    merge :: Maybe (Version, String) -> String -> Version
-    merge Nothing c = vid
-    merge (Just (ovid, c1)) c2 = if (c1 == c2) then ovid else vid
-    allFiles = Map.assocs nfis
-    calVersionInfo :: FilePath -> [((FilePath,ReplicaId),Version)] -> [((FilePath,ReplicaId),Version)]
-    calVersionInfo f [] = [((f, rid), vid)]
-    calVersionInfo f xs = ((f,rid), merge (find (\(f1,v) -> f1 == f && orid == rid) nfis) (fis Map.! f)) : (filter (\((_, orid), _) -> orid /= rid) xs)
-    finalVis = Map.fromList $ join $ map (\((f,orid),ovid) -> calVersionInfo f (filter (\((f1,_),_) ->f == f1) allFiles)) allFiles
+    calVersionCurrentReplica :: Maybe (Version, String) -> Maybe String -> Maybe Version
+    calVersionCurrentReplica (Just _) Nothing = Nothing
+    calVersionCurrentReplica Nothing (Just _) = Just vid
+    calVersionCurrentReplica (Just (ovid, c1)) (Just c2) = Just $ if (c1 == c2) then ovid else vid
+    calVersionCurrentReplica _ _ = error "not complete calVersionCurrentReplica"
+
+    findFileLocalVersion :: FilePath -> Maybe (Version, String)
+    findFileLocalVersion f = (,) <$> vis Map.!? (f, rid) <*> fis Map.!? f
+
+    findFileOtherVersion :: FilePath -> [((FilePath, ReplicaId), Version)]
+    findFileOtherVersion f = filter (\((f',rid'), _) -> f == f' && rid /= rid') (Map.assocs vis)
+
+    calVersionInfo :: FilePath -> Maybe String -> [((FilePath,ReplicaId),Version)]
+    calVersionInfo f nc = (maybeToList ((,) (f,rid) <$> calVersionCurrentReplica (findFileLocalVersion f) nc)) ++ findFileOtherVersion f
+    finalVis :: Map.Map (FilePath, ReplicaId) Version
+    finalVis = Map.fromList (join $ map (\f -> calVersionInfo f (nfis Map.!? f)) (nub $ Map.keys nfis ++ Map.keys fis))
 
 syncLocalDb :: FilePath -> IO Database
 syncLocalDb dir = do db <- localDatabase dir
--- lab3/trahs-1.0.0/test/Test.hs
+++ lab3/trahs-1.0.0/test/Test.hs
@@ -13,76 +13,66 @@ main = hspec $ describe "Testing Lab 3" $ do
     it "is inverse to show" $ property $
       \x -> (read . show) x == (x :: Int)
 
-  describe "dataBase" $ do
+  describe "localDatabase" $ do
     it "replica id is random in range" $ do
-      db <- dataBase "."
-      (db^.clientReplicaId) `shouldSatisfy` (\y -> y >= minBound && y <= maxBound)
+      db <- localDatabase "."
+      (db^.replicaId) `shouldSatisfy` (\y -> y >= minBound && y <= maxBound)
 
     it "local version is from 1" $ do
-      db <- dataBase "."
-      (db^.localVersion) `shouldBe` 1
+      db <- localDatabase "."
+      (db^.versionId) `shouldBe` Version 1
 
     it "version info should be empty when initial" $ do
-      db <- dataBase "."
-      (db^.versionInfos) `shouldBe` Map.empty
+      db <- localDatabase "."
+      (db^.dbVersionInfo) `shouldBe` Map.empty
 
     it "file info should be empty when initial" $ do
-      db <- dataBase "."
-      (db^.fileInfos) `shouldBe` Map.empty
+      db <- localDatabase "."
+      (db^.dbFileInfo) `shouldBe` Map.empty
 
-  describe "mergeInfo" $ do
-    let rid = 10
-        orid = 11
-        lversion = 3
-        localVersion = VersionInfo rid 1 False
-        increaseLocalVersion = VersionInfo rid lversion False
-        otherVersion = VersionInfo orid 12 False
-        md5 = "abc"
+  describe "mergeLocalInfo" $ do
+    let lrid = ReplicaId 10
+        orid = ReplicaId 11
+        file1 = "file1"
+        file2 = "file2"
+        versionForLocalFile1 = Version 3
+        versionForOtherFile1 = Version 10
+        versionForLocalFile2 = Version 4
+        currentVersion = Version 5
+        localVersionInfo = Map.fromList [((file1, lrid), versionForLocalFile1)
+                                        ,((file1, orid), versionForOtherFile1)
+                                        ,((file2, lrid), versionForLocalFile2)]
+        file1Hash = "hash1"
+        file2Hash = "hash2"
+        localFileInfo = Map.fromList [(file1, file1Hash)
+                                     ,(file2, file2Hash)]
+        localDb = Database lrid currentVersion localVersionInfo localFileInfo
     it "keep version when file not change" $ do
-      let localFileInfo = Map.fromList [("file1", FileInfo md5)
-                                       ,("file2", FileInfo md5)]
-          vis = Map.fromList [("file1", [localVersion, otherVersion])
-                             ,("file2", [localVersion])]
-          db = Database rid lversion vis localFileInfo
-          ndb = mergeInfo db localFileInfo
-      ndb `shouldBe` db
+      let ndb = mergeLocalInfo localDb localFileInfo
+      ndb `shouldBe` localDb
 
-    it "increase version when old file change" $ do
-      let oldFileInfo = Map.fromList [("file1", FileInfo md5)
-                                     ,("file2", FileInfo md5)]
-          ovis = Map.fromList [("file1", [localVersion, otherVersion])
-                              ,("file2", [localVersion])]
-          nmd5 = "sk"
-          newFileInfo = Map.fromList [("file1", FileInfo nmd5)
-                                     ,("file2", FileInfo nmd5)]
-          db = Database rid lversion ovis oldFileInfo
-          ndb = mergeInfo db newFileInfo
-      ndb `shouldBe` Database rid lversion
-        (Map.fromList [("file1", [VersionInfo rid lversion False, otherVersion])
-                      ,("file2", [VersionInfo rid lversion False])])
-        newFileInfo
+    it "increase current replica version and keep other replica version when old file change" $ do
+      let newHash = "newHash"
+          newFileInfo = Map.fromList [(file1, newHash)
+                                     ,(file2, newHash)]
+          ndb = mergeLocalInfo localDb newFileInfo
+      ndb `shouldBe` Database lrid currentVersion (Map.fromList [((file1, lrid), currentVersion)
+                                        ,((file1, orid), versionForOtherFile1)
+                                        ,((file2, lrid), currentVersion)]) newFileInfo
 
-    it "mark delete when old file deleted" $ do
-      let oldFileInfo = Map.fromList [("file1", FileInfo md5)
-                                     ,("file2", FileInfo md5)]
-          ovis = Map.fromList [("file1", [localVersion, otherVersion])
-                              ,("file2", [localVersion])]
-          newFileInfo = Map.fromList []
-          db = Database rid lversion ovis oldFileInfo
-          ndb = mergeInfo db newFileInfo
-      ndb `shouldBe` Database rid lversion
-        (Map.fromList [("file1", [VersionInfo rid lversion True, otherVersion])
-                      ,("file2", [VersionInfo rid lversion True])])
-        newFileInfo
+    it "delete version when old file deleted" $ do
+      let newFileInfo = Map.fromList []
+          ndb = mergeLocalInfo localDb newFileInfo
+      ndb `shouldBe` Database lrid currentVersion (Map.fromList [((file1, orid), versionForOtherFile1)]) newFileInfo
 
     it "add new file version when add file" $ do
-      let oldFileInfo = Map.fromList [("file2", FileInfo md5)]
-          ovis = Map.fromList [("file2", [localVersion])]
-          newFileInfo = Map.fromList [("file1", FileInfo md5)
-                                     ,("file2", FileInfo md5)]
-          db = Database rid lversion ovis oldFileInfo
-          ndb = mergeInfo db newFileInfo
-      ndb `shouldBe` Database rid lversion
-        (Map.fromList [("file1", [VersionInfo rid lversion False])
-                      ,("file2", [localVersion])])
-        newFileInfo
+      let file3 = "file3"
+          file3Hash = "file3Hash"
+          newFileInfo = Map.fromList [(file1, file1Hash)
+                                     ,(file2, file2Hash)
+                                     ,(file3, file3Hash)]
+          ndb = mergeLocalInfo localDb newFileInfo
+      ndb `shouldBe` Database lrid currentVersion (Map.fromList [((file1, lrid), versionForLocalFile1)
+                                        ,((file1, orid), versionForOtherFile1)
+                                        ,((file2, lrid), versionForLocalFile2)
+                                        ,((file3, lrid), currentVersion)]) newFileInfo
